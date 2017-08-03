6ac0d5f3b8c1d6ce8b01fef1df4ad1a027eca47c
Denys Kutsak <denyskutsak@gmail.com>
2017-08-03 15:43:49 +0300

    Bug fix

--- ReportsOrganizer/ReportsOrganaizer.UI/ViewModel/BaseViewModel.cs
+++ ReportsOrganizer/ReportsOrganaizer.UI/ViewModel/BaseViewModel.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ReportsOrganaizer.UI.ViewModel
 {
-    public class BaseViewModel
+    public class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
