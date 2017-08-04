2a76985ff3d9278e3d25b091478dca99cb16ba6e
Denys Kutsak <denyskutsak@gmail.com>
2017-08-04 16:07:42 +0300

    Single Instance Application

--- ReportsOrganizer/ReportsOrganizer.UI/App.xaml.cs
+++ ReportsOrganizer/ReportsOrganizer.UI/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Windows;
 
 namespace ReportsOrganizer.UI
@@ -12,5 +13,14 @@ namespace ReportsOrganizer.UI
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex mutex = new Mutex(true, "{CA13A683-04A7-41E5-BFB1-43D22BADADB7}");
+
+        public App()
+        {
+            if (!mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Current.Shutdown();
+            }
+        }
     }
 }
