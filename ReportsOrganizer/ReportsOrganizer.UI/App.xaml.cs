71b5f0d0ec448cfc68ce35f93b55c99d7edb0eb3
Denys Kutsak <denyskutsak@gmail.com>
2017-08-08 17:35:54 +0300

    Fixed bug

--- ReportsOrganizer/ReportsOrganizer.UI/App.xaml.cs
+++ ReportsOrganizer/ReportsOrganizer.UI/App.xaml.cs
@@ -11,6 +11,7 @@ using System.Globalization;
 using WPFLocalizeExtension.Engine;
 using ReportsOrganizer.UI.ViewModels;
 using SimpleInjector;
+using System.ComponentModel;
 
 namespace ReportsOrganizer.UI
 {
@@ -19,11 +20,15 @@ namespace ReportsOrganizer.UI
     /// </summary>
     public partial class App : Application
     {
-        private static Mutex mutex = new Mutex(true, "{CA13A683-04A7-41E5-BFB1-43D22BADADB7}");
+        private static Mutex _mutex = new Mutex(true, "{CA13A683-04A7-41E5-BFB1-43D22BADADB7}");
+
+        private readonly DependencyObject _dummy = new DependencyObject();
+
+        private bool IsInDesignMode => DesignerProperties.GetIsInDesignMode(_dummy);
 
         public App()
         {
-            if (!mutex.WaitOne(TimeSpan.Zero, true))
+            if (!_mutex.WaitOne(TimeSpan.Zero, true) && !IsInDesignMode)
             {
                 Current.Shutdown();
             }
