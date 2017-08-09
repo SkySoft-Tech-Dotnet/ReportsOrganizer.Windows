7422a025e91ccaa38edeff14e1ec7c8c43e1502c
Denys Kutsak <denyskutsak@gmail.com>
2017-08-09 15:39:58 +0300

    Updated style in Settings

--- ReportsOrganizer/ReportsOrganizer.UI/ViewModels/SettingsViewModel.cs
+++ ReportsOrganizer/ReportsOrganizer.UI/ViewModels/SettingsViewModel.cs
@@ -12,6 +12,9 @@ namespace ReportsOrganizer.UI.ViewModels
     public interface ISettingsViewModel
     {
         ICommand BackCommand { get; }
+
+        IList<int> ListHours { get; }
+        IList<int> ListMinutes { get; }
     }
 
     public class SettingsViewModel : BaseViewModel, ISettingsViewModel
@@ -19,11 +22,16 @@ namespace ReportsOrganizer.UI.ViewModels
         private INavigationService navigation;
 
         public ICommand BackCommand { get; private set; }
+        public IList<int> ListHours { get; }
+        public IList<int> ListMinutes { get; }
 
         public SettingsViewModel(INavigationService navigationService)
         {
             navigation = navigationService;
             BackCommand = new RelayCommand(BackAction, true);
+
+            ListHours = new List<int>(Enumerable.Range(0, 24));
+            ListMinutes = new List<int>(Enumerable.Range(0, 59));
         }
 
         private void BackAction(object sender)
