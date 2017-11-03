8b597307f2d47a1609c38f2b33dcd1caf7f8c721
Maks <maksy@DESKTOP-JDJVT41>
2017-11-03 17:28:16 +0200

    appsettings are rewriting after property changing now

--- ReportsOrganizer/ReportsOrganizer.UI/ViewModels/SettingsViewModel.cs
+++ ReportsOrganizer/ReportsOrganizer.UI/ViewModels/SettingsViewModel.cs
@@ -8,6 +8,7 @@ using ReportsOrganizer.UI.Command;
 using ReportsOrganizer.UI.Services;
 using ReportsOrganizer.UI.Models;
 using ReportsOrganizer.Core.Services;
+using System.ComponentModel;
 
 namespace ReportsOrganizer.UI.ViewModels
 {
@@ -29,34 +30,28 @@ namespace ReportsOrganizer.UI.ViewModels
         public ICommand BackCommand { get; private set; }
         public IList<int> ListHours { get; }
         public IList<int> ListMinutes { get; }
-
-        private bool _startMinimized;
+        
         public bool StartMinimized
         {
-            get => _startMinimized;
+            get => _configurationService.Value.General.StartMinimized;
             set
             {
-                _startMinimized = value;
-                _configurationService.Value.General.StartMinimized = _startMinimized;
-                Task.Run(async () => await _configurationService.UpdateAsync());
+                _configurationService.Value.General.StartMinimized = value;
                 NotifyPropertyChanged(nameof(StartMinimized));
             }
         }
-
-        private bool _startupWithWindows;
+        
         public bool StartupWithWindows
         {
             get
             {
-                return _startupWithWindows;
+                return _configurationService.Value.General.StartupWithWindows;
             }
             set
             {
-                _startupWithWindows = value;
-                if (!_startupWithWindows)
-                    StartMinimized = _startupWithWindows;
-                _configurationService.Value.General.StartupWithWindows = _startupWithWindows;
-                Task.Run(async () => await _configurationService.UpdateAsync());
+                if (!value)
+                    StartMinimized = value;
+                _configurationService.Value.General.StartupWithWindows = value;
                 NotifyPropertyChanged(nameof(StartupWithWindows));
             }
         }
@@ -70,11 +65,19 @@ namespace ReportsOrganizer.UI.ViewModels
 
             ListHours = new List<int>(Enumerable.Range(0, 24));
             ListMinutes = new List<int>(Enumerable.Range(0, 59));
+
+            PropertyChanged += UpdateSettings;
         }
 
         private void BackAction(object sender)
         {
             _navigationService.NavigateToHome();
+        }
+
+        private void UpdateSettings(object sender, PropertyChangedEventArgs e)
+        {
+            Task.Run(async () => await _configurationService.UpdateAsync());
+            //await _configurationService.UpdateAsync();
         }
     }
 }
