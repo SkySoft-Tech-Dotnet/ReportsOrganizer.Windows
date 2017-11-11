058dd5a25d055048797b1708e78996e8ea6f5199
Denys Kutsak <denyskutsak@gmail.com>
2017-11-11 18:42:39 +0200

    Updated window styles

---
+++ ReportsOrganizer/ReportsOrganizer.UI/Extensions/DependencyObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace ReportsOrganizer.UI.Extensions
+{
+    public static class DependencyObjectExtensions
+    {
+        public static TProperty GetValue<TProperty>(
+            this DependencyObject dependencyObject, DependencyProperty dependencyProperty)
+        {
+            return (TProperty)dependencyObject.GetValue(dependencyProperty);
+        }
+    }
+}
