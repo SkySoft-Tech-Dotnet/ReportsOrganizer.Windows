a8dab8638a54072198fe24de0408f5a2ecc4f009
Denys Kutsak <denyskutsak@gmail.com>
2017-11-12 20:11:56 +0200

    Updated method UpdateAsync

--- ReportsOrganizer/ReportsOrganizer.Core.Test/Services/ApplicationOptions.cs
+++ ReportsOrganizer/ReportsOrganizer.Core.Test/Services/ApplicationOptions.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json.Linq;
 using ReportsOrganizer.Core.Infrastructure;
 using ReportsOrganizer.Core.Services;
 using ReportsOrganizer.UI.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
@@ -27,15 +28,15 @@ namespace ReportsOrganizer.Core.Test.Services
             Assert.IsTrue(settings["general"]?.Value<string>("language") == language);
         }
 
-        [DataTestMethod, DisplayName("ApplicationOptions:UpdateAsync:Threads")]
-        [DataRow("ru"), DataRow("ua"), DataRow("en")]
-        public async Task UpdateAsyncThreads(string language)
+        [DataTestMethod]
+        [DataRow("ru", 5), DataRow("ua", 10), DataRow("en", 50)]
+        public async Task UpdateAsyncThreads(string language, int taskCount)
         {
             var service = IoC.Container.GetInstance<IApplicationOptions<ApplicationSettings>>();
             service.Value.General.Language = language;
 
             var tasks = new List<Task>();
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < taskCount; i++)
             {
                 tasks.Add(service.UpdateAsync(default(CancellationToken)));
             }
@@ -43,6 +44,33 @@ namespace ReportsOrganizer.Core.Test.Services
 
             var settings = JObject.Parse(File.ReadAllText("appsettings.test.json"));
             Assert.IsTrue(settings["general"]?.Value<string>("language") == language);
+        }
+
+        [DataTestMethod]
+        [DataRow("ru", 5), DataRow("ua", 10), DataRow("en", 50)]
+        public async Task UpdateAsyncThreadsDelay(string language, int taskCount)
+        {
+            var service = IoC.Container.GetInstance<IApplicationOptions<ApplicationSettings>>();
+            service.Value.General.Language = language;
+
+            var tasks = new List<Task>();
+            var random = new Random();
+
+            for (int i = 0; i < taskCount; i++)
+            {
+                tasks.Add(UpdateAsyncThreadsDelay(service, random.Next(0, 1000)));
+            }
+            await Task.WhenAll(tasks);
+
+            var settings = JObject.Parse(File.ReadAllText("appsettings.test.json"));
+            Assert.IsTrue(settings["general"]?.Value<string>("language") == language);
+        }
+
+        private async Task UpdateAsyncThreadsDelay(
+            IApplicationOptions<ApplicationSettings> service, int delay)
+        {
+            await Task.Delay(delay);
+            await service.UpdateAsync(default(CancellationToken));
         }
     }
 }
--- ReportsOrganizer/ReportsOrganuzer.Core/Services/ApplicationOptions.cs
+++ ReportsOrganizer/ReportsOrganuzer.Core/Services/ApplicationOptions.cs
@@ -1,4 +1,6 @@
 ﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -18,22 +20,26 @@ namespace ReportsOrganizer.Core.Services
         where T : class
     {
         private string _path;
-        private CancellationTokenSource _lastCancellationTokenSource;
+        private Stack<CancellationTokenSource> _cancellationTokensSource;
+
         private object _synchronizing;
+        private object _stackSynchronizing;
 
         public T Value { get; set; }
 
         public ApplicationOptions(string path)
         {
             _path = path;
-            _lastCancellationTokenSource = new CancellationTokenSource();
+            _cancellationTokensSource = new Stack<CancellationTokenSource>();
+
             _synchronizing = new object();
+            _stackSynchronizing = new object();
         }
 
         public async Task LoadAsync(CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
-            
+
             using (StreamReader reader = File.OpenText(_path))
             {
                 var content = await reader.ReadToEndAsync();
@@ -44,10 +50,17 @@ namespace ReportsOrganizer.Core.Services
         public Task UpdateAsync(CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
+            var cancellationTokenSource = new CancellationTokenSource();
 
-            if (!_lastCancellationTokenSource.IsCancellationRequested)
+            lock (_stackSynchronizing)
             {
-                _lastCancellationTokenSource.Cancel();
+                while (_cancellationTokensSource.Count != 0)
+                {
+                    _cancellationTokensSource.Pop().Cancel();
+                }
+
+                _cancellationTokensSource.Push(cancellationTokenSource);
+                cancellationToken.Register(cancellationTokenSource.Cancel);
             }
 
             var content = JsonConvert.SerializeObject(Value);
@@ -55,22 +68,19 @@ namespace ReportsOrganizer.Core.Services
 
             lock (_synchronizing)
             {
-                _lastCancellationTokenSource = new CancellationTokenSource();
-                cancellationToken.Register(_lastCancellationTokenSource.Cancel);
-
                 using (FileStream stream = File.Open(_path, FileMode.Create))
                 {
                     stream.Seek(0, SeekOrigin.End);
-                    stream.WriteAsync(
-                        contentEncode, 0, contentEncode.Length, _lastCancellationTokenSource.Token).Wait();
+                    try
+                    {
+                        stream.WriteAsync(
+                            contentEncode, 0, contentEncode.Length, cancellationTokenSource.Token).Wait();
+                    }
+                    catch (AggregateException) { }
                 }
             }
 
-            if (!_lastCancellationTokenSource.IsCancellationRequested)
-            {
-                _lastCancellationTokenSource.Cancel();
-            }
-
+            cancellationTokenSource.Cancel();
             return Task.CompletedTask;
         }
     }
