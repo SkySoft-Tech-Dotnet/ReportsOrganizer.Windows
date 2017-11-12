c6c28ef1c0e65d4efaa938d221eeea27a71d591c
Denys Kutsak <denyskutsak@gmail.com>
2017-11-12 18:56:12 +0200

    Added class for work with application settings

---
+++ ReportsOrganizer/ReportsOrganizer.Core.Test/Services/ApplicationOptions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using ReportsOrganizer.Core.Infrastructure;
+using ReportsOrganizer.Core.Services;
+using ReportsOrganizer.UI.Models;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReportsOrganizer.Core.Test.Services
+{
+    [TestClass]
+    public class ApplicationOptions
+    {
+        [DataTestMethod, DisplayName("ApplicationOptions:UpdateAsync")]
+        [DataRow("ru"), DataRow("ua"), DataRow("en")]
+        public async Task UpdateAsync(string language)
+        {
+            var service = IoC.Container.GetInstance<IApplicationOptions<ApplicationSettings>>();
+
+            service.Value.General.Language = language;
+            await service.UpdateAsync(default(CancellationToken));
+
+            var settings = JObject.Parse(File.ReadAllText("appsettings.test.json"));
+            Assert.IsTrue(settings["general"]?.Value<string>("language") == language);
+        }
+
+        [DataTestMethod, DisplayName("ApplicationOptions:UpdateAsync:Threads")]
+        [DataRow("ru"), DataRow("ua"), DataRow("en")]
+        public async Task UpdateAsyncThreads(string language)
+        {
+            var service = IoC.Container.GetInstance<IApplicationOptions<ApplicationSettings>>();
+            service.Value.General.Language = language;
+
+            var tasks = new List<Task>();
+            for (int i = 0; i < 50; i++)
+            {
+                tasks.Add(service.UpdateAsync(default(CancellationToken)));
+            }
+            await Task.WhenAll(tasks);
+
+            var settings = JObject.Parse(File.ReadAllText("appsettings.test.json"));
+            Assert.IsTrue(settings["general"]?.Value<string>("language") == language);
+        }
+    }
+}
---
+++ ReportsOrganizer/ReportsOrganuzer.Core/Services/ApplicationOptions.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReportsOrganizer.Core.Services
+{
+    public interface IApplicationOptions<T>
+        where T : class
+    {
+        T Value { get; }
+
+        Task UpdateAsync(CancellationToken cancellationToken);
+    }
+
+    internal class ApplicationOptions<T> : IApplicationOptions<T>
+        where T : class
+    {
+        private string _path;
+        private CancellationTokenSource _lastCancellationTokenSource;
+        private object _synchronizing;
+
+        public T Value { get; set; }
+
+        public ApplicationOptions(string path)
+        {
+            _path = path;
+            _lastCancellationTokenSource = new CancellationTokenSource();
+            _synchronizing = new object();
+        }
+
+        public async Task LoadAsync(CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            
+            using (StreamReader reader = File.OpenText(_path))
+            {
+                var content = await reader.ReadToEndAsync();
+                Value = JsonConvert.DeserializeObject<T>(content);
+            }
+        }
+
+        public Task UpdateAsync(CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (!_lastCancellationTokenSource.IsCancellationRequested)
+            {
+                _lastCancellationTokenSource.Cancel();
+            }
+
+            var content = JsonConvert.SerializeObject(Value);
+            var contentEncode = Encoding.ASCII.GetBytes(content);
+
+            lock (_synchronizing)
+            {
+                _lastCancellationTokenSource = new CancellationTokenSource();
+                cancellationToken.Register(_lastCancellationTokenSource.Cancel);
+
+                using (FileStream stream = File.Open(_path, FileMode.Create))
+                {
+                    stream.Seek(0, SeekOrigin.End);
+                    stream.WriteAsync(
+                        contentEncode, 0, contentEncode.Length, _lastCancellationTokenSource.Token).Wait();
+                }
+            }
+
+            if (!_lastCancellationTokenSource.IsCancellationRequested)
+            {
+                _lastCancellationTokenSource.Cancel();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
