bbb9f3fd219ac9b7458043fc5626d3c6222f6cc9
Maks <maksy@DESKTOP-JDJVT41>
2017-11-27 10:32:53 +0200

    Fixed closing of Notification window

--- ReportsOrganizer/ReportsOrganizer.UI/ViewModels/Windows/NotificationWindowViewModel.cs
+++ ReportsOrganizer/ReportsOrganizer.UI/ViewModels/Windows/NotificationWindowViewModel.cs
@@ -1,11 +1,34 @@
 ﻿using ReportsOrganizer.UI.Abstractions;
 using System;
+using System.ComponentModel;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Threading;
+using ReportsOrganizer.UI.Command;
 
 namespace ReportsOrganizer.UI.ViewModels.Windows
 {
     public class NotificationWindowViewModel : BaseViewModel
     {
+        private Visibility _windowVisibility;
+
+        public ICommand WindowClosingCommand { get; }
+
+        public Visibility WindowVisibility
+        {
+            get => _windowVisibility;
+            set => SetValue(ref _windowVisibility, value, nameof(WindowVisibility));
+        }
+
+        public NotificationWindowViewModel()
+        {
+            WindowClosingCommand = new RelayCommand(WindowClosingAction, true);
+        }
+
+        private void WindowClosingAction(object sender)
+        {
+            ((CancelEventArgs)sender).Cancel = true;
+            WindowVisibility = Visibility.Hidden;
+        }
     }
 }
